020e532cc79df6e221430a290c4d95a65dd28818
Rafael Jaén <rafitajaen@gmail.com>
2025-09-05 09:12:59 +0200

    WIP(main): Include License and CNAME

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Rafael Jaén';
-export const SITE_DESCRIPTION = 'Personal portfolio showcasing my work and projects.';
+export const SITE_TITLE = 'Rafael Jaén | Solopreneur | Building & Sharing in Public';
+export const SITE_DESCRIPTION = 'Exploring, building, and sharing in public. Hi!, I’m Rafael a full stack developer and solopreneur from Spain. My journey mixes projects, experiments, and lessons worth telling.';
