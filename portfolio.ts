f470c5c979da818cd78857897d701aef5d684268
Rafael Jaén <rafitajaen@gmail.com>
2026-01-13 14:13:46 +0100

    wip: include hostable.tools project

--- portfolio.ts
+++ portfolio.ts
@@ -9,7 +9,7 @@ export default {
     "name": "Rafael Jaén Profile",
     "avatar": "/hero.png",
     "title": "Hi, I'm Rafael — <span class=\"text-secondary\">A Full Stack web developer.</span>",
-    "description": `Welcome to my little corner of the internet.<br>I’m a software engineer and indie maker from Andalucía, Spain.<br>Currently building in public <a href="https://codeinspain.com?ref=${ref}" target="_blank" class="text-zinc-900 dark:text-zinc-100 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors">codeinspain</a> and <a href="https://dotnet.today?ref=${ref}" target="_blank" class="text-zinc-900 dark:text-zinc-100 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors">dotnet.today</a>.`,
+    "description": `Welcome to my little corner of the internet.<br>I’m a software engineer and indie maker from Andalucía, Spain.<br>Currently building in public <a href="https://opentodev.com?ref=${ref}" target="_blank" class="text-zinc-900 dark:text-zinc-100 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors">opentodev.com</a> and <a href="https://boilerplatr.com?ref=${ref}" target="_blank" class="text-zinc-900 dark:text-zinc-100 hover:text-zinc-900 dark:hover:text-zinc-100 transition-colors">boilerplatr.com</a>.`,
     "socialLinks": [
       { "name": "github", "url": "https://github.com/rafitajaen" },
       { "name": "x", "url": "https://x.com/rafitajaen" },
@@ -30,20 +30,20 @@ export default {
       "stack": "Next.js | React | Tailwind | shadcn/ui | PostgreSQL | Prisma | Better Auth | Stripe"
     },
     {
-      "name": "open<span class=\"text-emerald-600\">to</span>dev<span class=\"text-zinc-500 font-light\">.com</span>",
-      "status": "Beta",
-      "statusColor": "orange",
-      "url": "https://opentodev.com",
-      "description": "A platform designed to help developers find jobs that truly fit their skills and ambitions using smart filters. Beyond listings, we offer tools for interview prep and personal branding, connecting you with companies that value your expertise.",
-      "stack": "Typescript | .NET | MVC | Vite | Postgres | Redis | RabbitMQ | MJML | Docker"
+        "name": "open<span class=\"text-emerald-600\">to</span>dev<span class=\"text-zinc-500 font-light\">.com</span>",
+        "status": "In Public Beta",
+        "statusColor": "orange",
+        "url": "https://opentodev.com",
+        "description": "A platform designed to help developers find jobs that truly fit their skills and ambitions using smart filters. Beyond listings, we offer tools for interview prep and personal branding, connecting you with companies that value your expertise.",
+        "stack": "Typescript | .NET | MVC | Vite | Postgres | Redis | RabbitMQ | MJML | Docker"
     },
     {
-      "name": "code<span class=\"text-emerald-400\">in</span>spain<span class=\"text-zinc-500 font-light\">.com</span>",
-      "status": "Beta",
-      "statusColor": "orange",
-      "url": `https://codeinspain.com`,
-      "description": "A selective tech job board for Spain’s developer community. I highlight only the positions that meet clear standards of transparency, culture, and growth potential. Spend less time searching and more time advancing your career.",
-      "stack": "Typescript | .NET | MVC | Vite | Postgres | Redis | MJML | Docker"
+      "name": "hostable<span class=\"text-zinc-500 font-light\">.tools</span>",
+      "status": "Currently Building",
+      "statusColor": "rose",
+      "url": null,
+      "description": "A comprehensive directory for self-hosted software enthusiasts. Discover, compare, and deploy open-source alternatives to popular SaaS tools. Whether for privacy, control, or cost-savings, find the best self-hostable solutions for your personal server or business infrastructure.",
+      "stack": "Next.js | React | Tailwind | shadcn/ui | PostgreSQL | Prisma | Better Auth | Stripe"
     },
     {
       "name": "dotnet<span class=\"text-zinc-500 font-light\">.today</span>",
@@ -53,14 +53,22 @@ export default {
       "description": "Weekly newsletter and blog for .NET developers. I share tutorials, tools, and updates that help you code smarter, ship faster, and grow your skills with clear, practical content.",
       "stack": "Typescript | Astro | Listmonk | .NET"
     },
-    // {
-    //   "name": "indiemakers<span class=\"text-zinc-500 font-light\">.io</span>",
-    //   "status": "Private beta",
-    //   "statusColor": "orange",
-    //   "url": null,
-    //   "description": "Community platform for solopreneurs and indie makers to showcase products, share resources, and get feedback.",
-    //   "stack": "Typescript | .NET | MVC | Postgres | Docker"
-    // }
+    {
+      "name": "code<span class=\"text-emerald-400\">in</span>spain<span class=\"text-zinc-500 font-light\">.com</span>",
+      "status": "In Public Beta",
+      "statusColor": "orange",
+      "url": `https://codeinspain.com`,
+      "description": "A selective tech job board for Spain’s developer community. I highlight only the positions that meet clear standards of transparency, culture, and growth potential. Spend less time searching and more time advancing your career.",
+      "stack": "Typescript | .NET | MVC | Vite | Postgres | Redis | MJML | Docker"
+    },
+    {
+      "name": "indiemakers<span class=\"text-zinc-500 font-light\">.io</span>",
+      "status": "Cancelled",
+      "statusColor": "red",
+      "url": null,
+      "description": "Community platform for solopreneurs and indie makers to showcase products, share resources, and get feedback.",
+      "stack": "Typescript | .NET | MVC | Postgres | Docker"
+    }
   ],
   "experience": [
     {
@@ -72,7 +80,7 @@ export default {
       "badge": "Working",
       "icon": null,
       "description": "I’m independently designing and developing full-stack web projects, focusing on scalable architecture. I manage end-to-end product development, from idea validation to deployment and optimization. This work allows me to explore new frameworks, tools, and approaches while building impactful digital products.",
-      "stack": "TypeScript | .NET | MVC | Vite | SCSS | Docker | PostgreSQL | Redis | Figma"
+      "stack": "TypeScript | .NET | Next.js | Tailwind | Docker | PostgreSQL | Redis | RabbitMQ | Figma"
     },
     {
       "company": "Surcontrol",
